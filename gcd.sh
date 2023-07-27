c2048748a6853af100cb0d0eea3a69915e71b916
dkakinoki <d.kakinoki@gmail.com>
2023-07-27 06:46:21 +0000

    Add gcd.sh
     - 2つの自然数の最大公約数を求めるシェルスクリプト
     - 引数の数が2つであることを確認し、それ以外の場合はエラーメッセージを表示して終了
     - 引数が自然数かどうかをチェック
     - 2つの自然数の最大公約数を計算し、結果を表示

---
+++ gcd.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+# ヘルプメッセージを表示する関数
+print_usage() {
+    echo "使用方法: $0 <自然数1> <自然数2>"
+}
+
+# 引数が自然数かどうかをチェックする関数
+is_natural_number() {
+    re='^[0-9]+$'
+    if ! [[ $1 =~ $re ]] ; then
+        return 1
+    fi
+
+    if [ $1 -le 0 ]; then
+        return 1
+    fi
+
+    return 0
+}
+
+# 最大公約数を計算する関数
+gcd() {
+    local a=$1
+    local b=$2
+
+    while [ $b -ne 0 ]; do
+        local remainder=$((a % b))
+        a=$b
+        b=$remainder
+    done
+
+    echo $a
+}
+
+# 引数の数が2つでない場合はエラー終了
+if [ $# -ne 2 ]; then
+    echo "エラー: 2つの自然数を指定してください。"
+    print_usage
+    exit 1
+fi
+
+# 自然数でない引数が指定された場合はエラー終了
+if ! is_natural_number $1 || ! is_natural_number $2; then
+    echo "エラー: 自然数を指定してください。"
+    print_usage
+    exit 1
+fi
+
+# 最大公約数を計算して表示
+result=$(gcd $1 $2)
+echo "最大公約数: $result"
+
