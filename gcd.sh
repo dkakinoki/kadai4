b521c899c84fd49343130a57954963264a10e2e1
dkakinoki <d.kakinoki@gmail.com>
2023-07-29 07:01:38 +0000

    リファクタリング
    - コードを簡潔に

--- gcd.sh
+++ gcd.sh
@@ -1,20 +1,14 @@
 #!/bin/bash
 
-# 引数が自然数かどうかをチェックする関数
 is_natural_number() {
     re='^[0-9]+$'
-    if ! [[ $1 =~ $re ]] ; then
-        return 1
-    fi
-
-    if [ $1 -le 0 ]; then
+    if ! [[ $1 =~ $re ]] || [ $1 -le 0 ]; then
         return 1
     fi
 
     return 0
 }
 
-# 最大公約数を計算する関数
 gcd() {
     local a=$1
     local b=$2
@@ -28,19 +22,16 @@ gcd() {
     echo $a
 }
 
-# 引数の数が2つでない場合はエラー終了
 if [ $# -ne 2 ]; then
     echo "エラー: 2つの自然数を指定してください。" >&2
     exit 1
 fi
 
-# 自然数でない引数が指定された場合はエラー終了
 if ! is_natural_number $1 || ! is_natural_number $2; then
     echo "エラー: 自然数を指定してください。" >&2
     exit 1
 fi
 
-# 最大公約数を計算して表示
 result=$(gcd $1 $2)
 echo "最大公約数: $result"
 
--- test_gcd.sh
+++ test_gcd.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
-# シェルスクリプトのパス
 script_path="./gcd.sh"
 
-# 正しい挙動のテストケース
-echo "正しい入力のテスト:"
+echo "正常系:"
 output=$($script_path 2 4 2>&1)
 expected_output="最大公約数: 2"
 if [ "$output" = "$expected_output" ]; then
-    echo "正常に終了: $expected_output"
+    echo "正常終了: $expected_output"
 else
-    echo "エラー: 正しい挙動のテストに失敗しました。"
+    echo "エラー"
     echo "期待される出力: $expected_output"
     echo "実際の出力: $output"
     exit 1
@@ -19,9 +17,9 @@ fi
 output=$($script_path 100 25 2>&1)
 expected_output="最大公約数: 25"
 if [ "$output" = "$expected_output" ]; then
-    echo "正常に終了: $expected_output"
+    echo "正常終了: $expected_output"
 else
-    echo "エラー: 正しい挙動のテストに失敗しました。"
+    echo "エラー"
     echo "期待される出力: $expected_output"
     echo "実際の出力: $output"
     exit 1
@@ -30,9 +28,9 @@ fi
 output=$($script_path 7 13 2>&1)
 expected_output="最大公約数: 1"
 if [ "$output" = "$expected_output" ]; then
-    echo "正常に終了: $expected_output"
+    echo "正常終了: $expected_output"
 else
-    echo "エラー: 正しい挙動のテストに失敗しました。"
+    echo "エラー"
     echo "期待される出力: $expected_output"
     echo "実際の出力: $output"
     exit 1
@@ -41,9 +39,9 @@ fi
 output=$($script_path 24 36 2>&1)
 expected_output="最大公約数: 12"
 if [ "$output" = "$expected_output" ]; then
-    echo "正常に終了: $expected_output"
+    echo "正常終了: $expected_output"
 else
-    echo "エラー: 正しい挙動のテストに失敗しました。"
+    echo "エラー"
     echo "期待される出力: $expected_output"
     echo "実際の出力: $output"
     exit 1
@@ -52,59 +50,58 @@ fi
 output=$($script_path 1000000000 2000000000 2>&1)
 expected_output="最大公約数: 1000000000"
 if [ "$output" = "$expected_output" ]; then
-    echo "正常に終了: $expected_output"
+    echo "正常終了: $expected_output"
 else
-    echo "エラー: 正しい挙動のテストに失敗しました。"
+    echo "エラー"
     echo "期待される出力: $expected_output"
     echo "実際の出力: $output"
     exit 1
 fi
 
-# エラーケースのテスト
-echo "エラーケースのテスト:"
-# 引数の数が少ない場合のテスト
+echo "異常系:"
+# 引数の数が少ない
 output=$($script_path 2 2>&1)
 expected_output="エラー: 2つの自然数を指定してください。"
 if [ "$output" = "$expected_output" ]; then
-    echo "引数の数が少ない場合のテストに成功しました。"
+    echo "正常終了"
 else
-    echo "エラー: 引数の数が少ない場合のテストに失敗しました。"
+    echo "エラー"
     echo "期待される出力: $expected_output"
     echo "実際の出力: $output"
     exit 1
 fi
 
-# 引数の数が多い場合のテスト
+# 引数の数が多い
 output=$($script_path 2 4 6 2>&1)
 expected_output="エラー: 2つの自然数を指定してください。"
 if [ "$output" = "$expected_output" ]; then
-    echo "引数の数が多い場合のテストに成功しました。"
+    echo "正常終了"
 else
-    echo "エラー: 引数の数が多い場合のテストに失敗しました。"
+    echo "エラー"
     echo "期待される出力: $expected_output"
     echo "実際の出力: $output"
     exit 1
 fi
 
-# 負の数が指定された場合のテスト
+# 負の数が指定された
 output=$($script_path -2 4 2>&1)
 expected_output="エラー: 自然数を指定してください。"
 if [ "$output" = "$expected_output" ]; then
-    echo "負の数が指定された場合のテストに成功しました。"
+    echo "正常終了"
 else
-    echo "エラー: 負の数が指定された場合のテストに失敗しました。"
+    echo "エラー"
     echo "期待される出力: $expected_output"
     echo "実際の出力: $output"
     exit 1
 fi
 
-# 小数が指定された場合のテスト
+# 小数が指定された
 output=$($script_path 2.5 4 2>&1)
 expected_output="エラー: 自然数を指定してください。"
 if [ "$output" = "$expected_output" ]; then
-    echo "小数が指定された場合のテストに成功しました。"
+    echo "正常終了"
 else
-    echo "エラー: 小数が指定された場合のテストに失敗しました。"
+    echo "エラー"
     echo "期待される出力: $expected_output"
     echo "実際の出力: $output"
     exit 1
